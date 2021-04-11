e24be9991b3b314a993608088004fae7b0c403ce
David Colclazier <david@jdc.tech>
2021-04-11 14:09:11 -0600

    Initial commit 1.0

---
+++ SuperNova.AWS.Logging/Contract/IServiceLoggerFactory.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SuperNova.AWS.Logging.Contract
+{
+    public interface IServiceLoggerFactory
+    {
+        ILogger GetLogger(string category);
+    }
+
+}
